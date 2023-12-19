d64f9e1b526d61035276778d9be48d4f5fd87acc
sumanteja555 <sumanteja555@gmail.com>
2023-12-19 12:26:15 +0530

    first and final commit of the places to visit project with the help of react use effect an side effects

---
+++ src/components/Progress.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const TIMER = 3000;
+
+export default function Progress() {
+  const [remainingTime, setRemainingTime] = useState(TIMER);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <progress value={remainingTime} max={TIMER} />;
+}
